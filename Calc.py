803ee49b3d87fdd480d1b4ad996c67c4b92148ca
BechtelJustin <bechtel.justin16@icloud.com>
2025-03-03 09:19:13 -0500

    Made it loop but only once

--- Calc.py
+++ Calc.py
@@ -1,12 +1,30 @@
-print("Welcome to calculator")
-a = input("Enter first number: ")
-b = input("Enter second number: ")
-c = input("+, -, *, /")
-if c == "+":
-    print(int(a)+int(b))
-if c == "-":
-    print(int(a)-int(b))
-if c == "*":
-    print(int(a)*int(b))
-if c == "/":
-    print(int(a)/int(b))
+def Calculator():
+    print("Welcome to calculator")
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = input("+, -, *, /")
+    if c == "+":
+        print(int(a)+int(b))
+    if c == "-":
+        print(int(a)-int(b))
+    if c == "*":
+        print(int(a)*int(b))
+    if c == "/":
+        print(int(a)/int(b))
+
+
+Calculator()
+
+
+def question():
+    print("Continue?")
+    input("yes or no?: ")
+    yes = 1
+    no = 2
+    if 1:
+        Calculator()
+    if 2:
+        print("Bye, bye")
+
+
+question()
