669948a0c1ce09507a6eefb57e71430834bbea88
BechtelJustin <bechtel.justin16@icloud.com>
2025-03-03 12:43:56 -0500

    Cleaned up code

--- Calc.py
+++ Calc.py
@@ -1,3 +1,6 @@
+print("Welcome to calculator")
+
+
 def question():
     print("Continue?")
     i = input("yes = 1 or no = 0?: ")
@@ -7,29 +10,30 @@ def question():
 
 
 def calculator():
-    print("Welcome to calculator")
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+    number_1 = float(input("Enter first number: "))
+    number_2 = float(input("Enter second number: "))
     c = input("+, -, *, /")
     if c == "+":
-        print(a+b)
+        print(number_1+number_2)
     if c == "-":
-        print(a-b)
+        print(number_1-number_2)
     if c == "*":
-        print(a*b)
+        print(number_1*number_2)
     if c == "/":
-        if a == 0:
-            a1010()
-        elif b == 0:
-            a1010()
+        if number_1 == 0:
+            errorcode_a1010()
+        elif number_2 == 0:
+            errorcode_a1010()
         else:
-            print(a/b)
+            print(number_1/number_2)
     question()
 
 
-def a1010():
+def errorcode_a1010():
     print("Cannot Divide 0")
     calculator()
 
 
 calculator()
+
+# make error codes for if someone puts in a word for number_1 or 2 then send them back to enter the number
