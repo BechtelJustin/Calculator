35feb42637eb461e0335a2acb0ca2275b46f3f0a
BechtelJustin <bechtel.justin16@icloud.com>
2025-03-06 13:57:29 -0500

    Got my symbol detection to where I want it.

--- Calc.py
+++ Calc.py
@@ -1,6 +1,3 @@
-print("Welcome to calculator")
-
-
 def question_1():
     print("Continue?")
     i = input("Yes or No?: ").lower()
@@ -16,58 +13,62 @@ def question_1():
 
 def number_1():
     number_1v = (input("Enter first number: "))
-    print(number_1v)
     return number_1v
+
+
+def calculation():
+    calculation_s = input("+, -, *, /: ")
+    return calculation_s
 
 
 def number_2():
     number_2v = (input("Enter second number: "))
-    print(number_2v)
     return number_2v
 
 
 def calculator():
 
     number_1v = number_1()
-    while not number_1v.isdigit():
+    while not number_1v.isdigit() and number_1v.isdecimal():
         print((number_1v), "Is not valid number")
         number_1v = number_1()
+
     number_1v = float(number_1v)
 
-    calculation = input("+, -, *, /: ")
+    calculation_s = calculation()
+    symbol_count = calculation_s
 
     number_2v = number_2()
-    while not number_2v.isdigit():
+    while not number_2v.isdigit() and number_2v.isdecimal():
         print((number_2v), "Is not valid number")
         number_2v = number_2()
     number_2v = float(number_2v)
 
-    if calculation == "+":
+    symbol_count = len(calculation_s.split("+"))-1
+    symbol_count = int(symbol_count)
+    if symbol_count == 1:
         print(number_1v+number_2v)
-
-    if calculation == "-":
-        print(number_1v-number_2v)
-
-    if calculation == "*":
-        print(number_1v*number_2v)
-
-    if calculation == "/":
-        if number_1v == 0:
-            print("cannot divide by 0")
-            calculator()
-
-        elif number_2v == 0:
-            print("cannot divide by 0")
-            calculator()
-
+    else:
+        symbol_count = len(calculation_s.split("-"))-1
+        symbol_count = int(symbol_count)
+        if symbol_count == 1:
+            print(number_1v-number_2v)
         else:
-            print(number_1v/number_2v)
+            symbol_count = len(calculation_s.split("*"))-1
+            symbol_count = int(symbol_count)
+            if symbol_count == 1:
+                print(number_1v*number_2v)
+            else:
+                symbol_count = len(calculation_s.split("/"))-1
+                symbol_count = int(symbol_count)
+                if symbol_count == 1:
+                    if number_2v == 0 or number_1v == 0 and number_2v == 0:
+                        print("cannot divide by 0")
+                    else:
+                        print(number_1v/number_2v)
 
     question_1()
 
 
+print("Welcome to calculator")
 calculator()
-
-# make error codes for if someone puts in a word for number_1 or 2 then send them back to enter the number
-# made it restart from the start would like to send back to whatever step they were on before
-# fix /, 100/0 should = 0
